6f2a94c2a60b45fcc50c6c0450e72943832613fe
Gadyel Navarro <138814312+gN877nul@users.noreply.github.com>
2026-08-18 23:35:55 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using A.S.H.L.E.E._alfa.Core;
+
+namespace A.S.H.L.E.E._alfa
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "ASHLEE ARMOR OS";
+
+            AshleeSystem ashlee = new AshleeSystem();
+
+            await ashlee.InitializeAsync();
+
+            await ashlee.StartAsync();
+        }
+    }
+}
